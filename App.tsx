70008afe898f43ed0e8c80767cdf945c986a2bdc
Adam Gorring <adamgorring@gmail.com>
2024-01-04 12:18:12 +1000

    Created simple background colour changer

--- App.tsx
+++ App.tsx
@@ -1,20 +1,50 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { useState } from "react";
+import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
 
 export default function App() {
+  const [bgColor, setBgColor] = useState("#ffffff");
+
+  const generateColor = () => {
+    const hexRange = "0123456789ABCDEF";
+    let color = "#";
+
+    for (let i = 0; i < 6; i++) {
+      color += hexRange[Math.floor(Math.random() * 16)];
+    }
+    setBgColor(color);
+  };
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <>
+      <StatusBar backgroundColor={bgColor} />
+      <View style={[styles.container, { backgroundColor: bgColor }]}>
+        <TouchableOpacity onPress={generateColor}>
+          <View style={styles.actionBtn}>
+            <Text style={styles.actionBtnTxt}>Press me</Text>
+          </View>
+        </TouchableOpacity>
+      </View>
+    </>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  actionBtn: {
+    borderRadius: 12,
+    backgroundColor: "#6A1B4D",
+    paddingVertical: 10,
+    paddingHorizontal: 40,
+  },
+  actionBtnTxt: {
+    fontSize: 24,
+    color: "white",
+    textTransform: "uppercase",
   },
 });
